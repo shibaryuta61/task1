1b886f4513149231ed88f83ea932d74428146f71
EC2 Default User <ec2-user@ip-172-31-16-65.us-east-2.compute.internal>
2020-03-28 00:58:15 +0000

    laravel15

---
+++ edit.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.admin')
+@section('title', 'プロフィールの編集')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール編集</h2>
+                
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    
+                   <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名（name）</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    
+                     <div class="form-group row">
+                        <label class="col-md-2" for="gender">性別（gender）</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2" for="hobby">趣味（hobby）</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2" for="introduction">自己紹介欄（introduction）</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                    <input type="submit" class="btn btn-primary" value="更新">
+                    </div>
+                    
+                     {{ csrf_field() }}
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
